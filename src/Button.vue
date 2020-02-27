f249dc24246b8b52ff4ac138a593e2c48d40461f
KCVO1995 <943514589@qq.com>
2020-02-27 12:48:09 +0800

    加入 parcel，将button改成单文件模式

---
+++ src/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button class="g-button">按钮</button>
+  </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Button extends Vue {
+  }
+
+</script>
+
+<style lang='scss' scoped>
+  .g-button {
+    height: var(--button-height);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }  background: var(--button-bg);
+  }
+
+</style>
