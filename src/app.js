67f5e5bc05bab7422a6b7e6a4e309fa6060bff29
KCVO1995 <943514589@qq.com>
2020-02-27 21:04:26 +0800

    使用 chai.expect ,完4个单元测试

--- src/app.js
+++ src/app.js
@@ -16,3 +16,84 @@ new Vue({
     }
   }
 })
+
+
+import chai from 'chai'
+
+const {expect} = chai
+// 单元测试
+
+{
+  const vm = new Button({
+    propsData: {
+      icon: 'i-setting'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector("use")
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-setting')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const vm = new Button({
+    propsData: {
+      icon: 'i-setting',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector("use")
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const vm = new Button({
+    propsData: {
+      icon: 'i-setting',
+      iconPosition: 'left'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const vm = new Button({
+    propsData: {
+      icon: 'i-setting',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const vm = new Button({
+    propsData: {
+      icon: 'i-setting',
+      iconPosition: 'right',
+    }
+
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+
+  })
+  let button = vm.$el
+  button.click()
+}
