210d235bd6c84be2ed5343c8f73fd95e365e4f80
KCVO1995 <943514589@qq.com>
2020-03-01 23:14:51 +0800

    add Input.vue 并完成样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from "vue"
 import Button from "./Button"
 import Icon from "./Icon"
 import ButtonGroup from "./ButtonGroup"
+import Input from './Input'
 
 Vue.component("g-button", Button)
 Vue.component("g-icon", Icon)
 Vue.component("g-button-group", ButtonGroup)
+Vue.component('g-input', Input)
 new Vue({
   el: "#app",
   data() {
@@ -18,93 +20,93 @@ new Vue({
 })
 
 
-import chai from "chai"
-import spise from "chai-spies"
-
-chai.use(spise)
-const {expect} = chai
-// 单元测试
-try {
-  {
-    const vm = new Button({
-      propsData: {
-        icon: "i-setting"
-      }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector("use")
-    let href = useElement.getAttribute("xlink:href")
-    expect(href).to.eq("#i-setting")
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  {
-    const vm = new Button({
-      propsData: {
-        icon: "i-setting",
-        loading: true
-      }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector("use")
-    let href = useElement.getAttribute("xlink:href")
-    expect(href).to.eq("#i-loading")
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  {
-    const div = document.createElement("div")
-    document.body.appendChild(div)
-    const vm = new Button({
-      propsData: {
-        icon: "i-setting",
-        iconPosition: "left"
-      }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector("svg")
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq("1")
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  {
-    const div = document.createElement("div")
-    document.body.appendChild(div)
-    const vm = new Button({
-      propsData: {
-        icon: "i-setting",
-        iconPosition: "right"
-      }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector("svg")
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq("3")
-    vm.$el.remove()
-    vm.$destroy()
-  }
-  {
-    const vm = new Button({
-      propsData: {
-        icon: "i-setting",
-        iconPosition: "right",
-      }
-
-    })
-    vm.$mount()
-    let spy = chai.spy(() => {})
-    vm.$on("click", spy)
-    let button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-  }
-} catch (error) {
-  window.errors = [error]
-} finally {
-  if (window.errors) {
-    window.errors.forEach(error => {
-      console.error(error.message)
-    })
-  }
-}
+// import chai from "chai"
+// import spise from "chai-spies"
+//
+// chai.use(spise)
+// const {expect} = chai
+// // 单元测试
+// try {
+//   {
+//     const vm = new Button({
+//       propsData: {
+//         icon: "i-setting"
+//       }
+//     })
+//     vm.$mount()
+//     let useElement = vm.$el.querySelector("use")
+//     let href = useElement.getAttribute("xlink:href")
+//     expect(href).to.eq("#i-setting")
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     const vm = new Button({
+//       propsData: {
+//         icon: "i-setting",
+//         loading: true
+//       }
+//     })
+//     vm.$mount()
+//     let useElement = vm.$el.querySelector("use")
+//     let href = useElement.getAttribute("xlink:href")
+//     expect(href).to.eq("#i-loading")
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     const div = document.createElement("div")
+//     document.body.appendChild(div)
+//     const vm = new Button({
+//       propsData: {
+//         icon: "i-setting",
+//         iconPosition: "left"
+//       }
+//     })
+//     vm.$mount(div)
+//     let svg = vm.$el.querySelector("svg")
+//     let {order} = window.getComputedStyle(svg)
+//     expect(order).to.eq("1")
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     const div = document.createElement("div")
+//     document.body.appendChild(div)
+//     const vm = new Button({
+//       propsData: {
+//         icon: "i-setting",
+//         iconPosition: "right"
+//       }
+//     })
+//     vm.$mount(div)
+//     let svg = vm.$el.querySelector("svg")
+//     let {order} = window.getComputedStyle(svg)
+//     expect(order).to.eq("2")
+//     vm.$el.remove()
+//     vm.$destroy()
+//   }
+//   {
+//     const vm = new Button({
+//       propsData: {
+//         icon: "i-setting",
+//         iconPosition: "right",
+//       }
+//
+//     })
+//     vm.$mount()
+//     let spy = chai.spy(() => {})
+//     vm.$on("click", spy)
+//     let button = vm.$el
+//     button.click()
+//     expect(spy).to.have.been.called()
+//   }
+// } catch (error) {
+//   window.errors = [error]
+// } finally {
+//   if (window.errors) {
+//     window.errors.forEach(error => {
+//       console.error(error.message)
+//     })
+//   }
+// }
