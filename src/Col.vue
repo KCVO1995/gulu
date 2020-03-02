338627aa4d158e5291c0162d4cf9750ec62d7597
KCVO1995 <943514589@qq.com>
2020-03-02 18:30:21 +0800

    add Col.vue and Row.vue

---
+++ src/Col.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col">
+    <slot/>
+  </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Col extends Vue {
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+  .col {
+    background: #eeeeee;
+    border: 1px solid green;
+    height: 100px;
+    width: 100%;
+
+  }
+
+</style>
---
+++ src/Row.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <slot/>
+  </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Row extends Vue {
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+  .row {
+    display: flex;
+  }
+</style>
