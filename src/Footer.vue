ae62cbf5756c4d7239aabab79f4c4f8dc771e9df
KCVO1995 <943514589@qq.com>
2020-03-03 17:02:24 +0800

    add Header.vue Content.vue Footer.vue Sider.vue Layout.vue

---
+++ src/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Footer extends Vue {
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/Sider.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Aside extends Vue {
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
