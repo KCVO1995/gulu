18249358861b5b9aad9cabe39ff75e25716de612
KCVO1995 <943514589@qq.com>
2020-03-09 12:11:55 +0800

    button 组件改成使用scss变量

--- src/Button.vue
+++ src/Button.vue
@@ -10,11 +10,11 @@
 
 <script lang='ts'>
   import Vue from 'vue';
-  import gIcon from './Icon'
+  import gIcon from './Icon';
   import {Component, Prop} from 'vue-property-decorator';
 
   @Component({
-    components:{gIcon}
+    components: {gIcon}
   })
   export default class Button extends Vue {
     @Prop(String) icon: string | undefined;
@@ -31,23 +31,31 @@
 </script>
 
 <style lang='scss' scoped>
+  $button-height: 32px;
+  $font-size: 14px;
+  $button-bg: white;
+  $button-active-bg: #eee;
+  $border-radius: 4px;
+  $color: #333;
+  $border-color: #999;
+  $border-color-hover: #666;
   @keyframes spin {
     from {transform: rotate(0deg);}
     to {transform: rotate(360deg);}
   }
   .g-button {
-    height: var(--button-height);
-    font-size: var(--font-size);
+    height: $button-height;
+    font-size: $font-size;
     padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    background: $button-bg;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     vertical-align: middle;
-    &:hover {border-color: var(--border-color-hover);}
-    &:active {background: var(--button-active-bg);}
+    &:hover {border-color: $border-color-hover;}
+    &:active {background: $button-active-bg;}
     &:focus {outline: none;}
     > .content {order: 2}
     > .icon {
--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -24,19 +24,21 @@
 </script>
 
 <style lang='scss' scoped>
+  $border-radius: 4px;
   .global {
     display: inline-flex;
     vertical-align: middle;
     > .g-button {
       border-radius: 0;
-      &:not(first-child) {margin-left: -1px};
+      &:not(first-child) {margin-left: -1px}
+    ;
       &:first-child {
-        border-top-left-radius: var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
+        border-top-left-radius: $border-radius;
+        border-bottom-left-radius: $border-radius;
       }
       &:last-child {
-        border-top-right-radius: var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
+        border-top-right-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
       }
       &:hover {
         z-index: 1;
